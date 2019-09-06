893482f56ea9e3e571684a2978e771bd7a565b45
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-09-06 04:51:26 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -18,7 +18,7 @@ def apply_coupons(cart, coupons)
   # code here
   cart.each do |coupon|
     if cart.key.include? coupon[:item]
-      if cart[coupon]
+      if cart[coupon[:item][:count]] <=
 
 def apply_clearance(cart)
   # code here
