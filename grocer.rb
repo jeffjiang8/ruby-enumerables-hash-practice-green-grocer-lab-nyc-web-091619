ce5cd8ee43c69fd832b89cae59c5f40e9dc47eca
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-09-06 04:15:46 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -9,6 +9,8 @@ def consolidate_cart(cart)
         :price => item.values[0][:price],
         :clearance => item.values[0][:clearance]
       }
+    end 
+  end
 end
 
 def apply_coupons(cart, coupons)
