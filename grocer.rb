ca443515372f4bf3f0484df350e678d1be7a80a1
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-09-06 04:58:52 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -19,7 +19,7 @@ def apply_coupons(cart, coupons)
   cart.each do |coupon|
     if cart.key.include? coupon[:item]
       if cart[coupon[:item][:count]] >= count[:num]
-        dicounted = "#{[coupon[:item]]} W/ COUPON"
+        dicounted = "#{coupon[:item]} W/ COUPON"
         if cart[discounted]
           cart[discounted][:count] += coupon[:num]
         else
